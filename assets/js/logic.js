4399ece5f21be6a18fafb38b0bf492c553719281
JPDBrodeur <jbrodeur001@gmail.com>
2021-08-13 20:09:10 -0400

    advance from start to question #1

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,29 +1,44 @@
-var pageContentEl = document.querySelector('body');
 var timerEl = document.querySelector('.timer span');
 var wrapperEl = document.querySelector('div.wrapper');
 
+var currentQuestion = 0;
+
 function startScreen() {
     timerEl.textContent = '0'
-    // Start Div
-    var startingDivEl = document.createElement('div');
-    startingDivEl.className = 'start-screen';
+
+    // Start Screen Div
+    var startScreenEl = document.createElement('div');
+    startScreenEl.className = "start-screen"
+
     // Quiz Title
     var h1El = document.createElement('h1');
     h1El.className = 'centered-text';
     h1El.textContent = 'Coding Quiz Challenge';
+
     // Instructions
     var instructionsEl = document.createElement('p');
     instructionsEl.className = 'centered-text';
     instructionsEl.innerHTML = 'Try to answer the following code-related questions within the time limit.<br />Keep in mind that incorrect answers will penalize your score/time<br />by ten seconds!';
+    
     // Start Button
     var startBtnEl = document.createElement('button');
     startBtnEl.className = 'purple-btn centered-btn';
     startBtnEl.textContent = 'Start Quiz';
 
-    wrapperEl.appendChild(startingDivEl);
-    startingDivEl.appendChild(h1El);
-    startingDivEl.appendChild(instructionsEl);
-    startingDivEl.appendChild(startBtnEl);
+    // Add to screen
+    wrapperEl.appendChild(startScreenEl);
+    startScreenEl.appendChild(h1El);
+    startScreenEl.appendChild(instructionsEl);
+    startScreenEl.appendChild(startBtnEl);
+
+    startScreenEl.addEventListener('click', start);
+}
+
+function start() {
+    countdown();
+    var startScreenEl = document.querySelector('.start-screen')
+    startScreenEl.remove();
+    quiz (0);
 }
 
 function countdown() {
@@ -40,13 +55,55 @@ function countdown() {
     }, 1000);
 }
 
+function quiz(x) {
+    // Question Div
+    var questionDivEl = document.createElement('div');
+    questionDivEl.className = 'question-screen';
+    questionDivEl.setAttribute("data-prompt-number", (x + 1))
 
-function quiz() {
-    for (var i = 0; i < tasks.length; i++) {
-        
+    // Question Prompt
+    var h2El = document.createElement('h2');
+    h2El.className = 'question-prompt';
+    h2El.textContent = questions[x].q;
+
+    wrapperEl.appendChild(questionDivEl);
+    questionDivEl.appendChild(h2El);
+
+    var answer = questions[x].a;
+
+    // Multiple Choice Buttons
+    for (var i = 0; i < (questions[x].options.length); i++) {
+        var btnWrapEl = document.createElement('div');
+        btnWrapEl.className = 'btn-wrap';
+        var optionBtnEl = document.createElement('button');
+        optionBtnEl.className = 'purple-btn wide-btn';
+        optionBtnEl.textContent = (i + 1) + ". " + questions[x].options[i];
+        optionBtnEl.setAttribute('data-value', questions[x].options[i]);
+        questionDivEl.appendChild(btnWrapEl);
+        btnWrapEl.appendChild(optionBtnEl);
+    }
+
+    var correctEl = document.querySelector(".wide-btn[data-value='" + answer +"']");    
+    questionDivEl.addEventListener('click', selectedAnswer);
+}
+
+function selectedAnswer(event) {
+    var targetEl = event.target;
+    // edit button was clicked
+    if (targetEl.data-value.matches(answer)) {
+        console.log('Correct!')
+    }
+    // delete button was clicked
+    else {
+        console.log('Wrong!')
     }
 }
 
-pageContentEl.addEventListener('click', countdown);
+
+function advance(x) {
+    if (questions[x]) {
+
+    }
+}
 
 startScreen();
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,37 +1,22 @@
-questions = [
+var questions = [
     {q: "Commonly used data types DO Not Include:",
     a: "alerts",
-    option1: "strings",
-    option2: "booleans",
-    option3: "alerts",
-    option4: "numbers"
+    options: ["strings","booleans", "alerts", "numbers"]
     },
     {q: "The condition in an if / else statement is enclosed with __________.",
     a: "parenthesis",
-    option1: "quotes",
-    option2: "curly brackets",
-    option3: "parenthesis",
-    option4: "square brackets"
+    options: ["quotes", "curly brackets", "parenthesis", "square brackets"]
     },
     {q: "Arrays in JavaScript can be used to store __________.",
     a: "all of the above",
-    option1: "numbers and strings",
-    option2: "other arrays",
-    option3: "booleans",
-    option4: "all of the above"
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"]
     },
     {q: "String values must be enclosed within __________ when being assigned to variables.",
     a: "quotes",
-    option1: "commas",
-    option2: "curly brackets",
-    option3: "quotes",
-    option4: "parenthesis"
+    options: ["commas", "curly brackets", "quotes", "parenthesis"]
     },
     {q: "A very useful tool used during development and debugging for printing content to the debugger is:",
     a: "console.log",
-    option1: "JavaScript",
-    option2: "terminal/bash",
-    option3: "for loops",
-    option4: "console.log"
+    options: ["JavaScript", "terminal/bash", "for loops", "console.log"]
     },
 ]
